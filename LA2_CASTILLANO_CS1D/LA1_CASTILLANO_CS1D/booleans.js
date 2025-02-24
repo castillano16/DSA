d1f07c0ee416d457f96078a558a8cdd6468ece40
castillano16 <marbelencastillano3@gmail.com>
2025-02-24 13:38:58 +0800

    Add files via upload

---
+++ LA2_CASTILLANO_CS1D/LA1_CASTILLANO_CS1D/booleans.js
@@ -0,0 +1,12 @@
+// Creating variables as per instructions
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 16; // Replace 16 with your favorite number
+
+// Assigning undefined to favNumb
+favNumb = undefined;
+
+// Logging variables to the console
+console.log("isJsProgrammingLanguage:", isJsProgrammingLanguage);
+console.log("isJsHard:", isJsHard);
+console.log("favNumb:", favNumb);

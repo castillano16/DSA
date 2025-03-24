12e3d170ff88bf906b402191e1dbbeb8b330fe63
castillano16 <marbelencastillano3@gmail.com>
2025-03-24 08:43:56 +0800

    Update booleans.js

--- LA1_CASTILLANO_CS1D/booleans.js
+++ LA1_CASTILLANO_CS1D/booleans.js
@@ -1,12 +1,8 @@
-// Creating variables as per instructions
 let isJsProgrammingLanguage = true;
 let isJsHard = false;
-let favNumb = 16; // Replace 16 with your favorite number
-
-// Assigning undefined to favNumb
+let favNumb = 16; 
 favNumb = undefined;
 
-// Logging variables to the console
 console.log("isJsProgrammingLanguage:", isJsProgrammingLanguage);
 console.log("isJsHard:", isJsHard);
 console.log("favNumb:", favNumb);
